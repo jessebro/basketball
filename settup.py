eb1a99a206a86bc5e040655c1130622400ae75e8
Jesse Brown <jesseatbrown@gmail.com>
2021-11-17 11:48:13 +1100

    Positions cannot be taken twice

--- settup.py
+++ settup.py
@@ -6,26 +6,30 @@ from utils import Color
 import players
 
 def set_positions():
-	positions = ["point guard", "small forward", "center", "power forward", "shooting guard"]
 	keys = list(players.goodies.keys())
 	while True:
-		clear()
+		positions = ["point guard", "small forward", "center", "power forward", "shooting guard"]
+		positions_taken = []
+		options = ["1", "2", "3", "4", "5"]
 		for player in keys:
+			clear()
 			print(colour_it(players.goodies[player].name, Color.ALLY))
-			print(f"""Set where you want {colour_it(players.goodies[player].name, Color.ALLY)} to be. There are five positions.
-
-1. {positions[0].title()} (Offensive)
-2. {positions[1].title()} (Offensive)
-3. {positions[2].title()} (Neutral)
-4. {positions[3].title()} (Defensive)
-5. {positions[4].title()} (Defensive)
-
+			print(f"""Set where you want {colour_it(players.goodies[player].name, Color.ALLY)} to be.""")
+			counter = 0
+			for position in positions:
+				if position not in positions_taken:
+					counter += 1
+					print(f"""{counter}. {position.title()} """)
+			print(f"""
 This player's stats are:
 Offense: {players.goodies[player].offense}
 Defence: {players.goodies[player].defence}
-Speed: {players.goodies[player].speed}
-""")
+Speed: {players.goodies[player].speed}""")
 			position = input_stuff("""
 Select the corresponding number of a position.
->>> """, ["1", "2", "3", "4", "5"])
+>>> """, options)
 			players.goodies[player].position = positions[int(position) - 1]
+			if players.goodies[player].position != "":
+				positions_taken.append(players.goodies[player].position)
+				positions.remove(players.goodies[player].position)
+				options.remove(options[len(options) - 1])
